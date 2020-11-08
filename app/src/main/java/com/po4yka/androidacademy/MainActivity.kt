c93a7a0c4e5122bbe7c80b3ea3425a3838784875
Nikita Pochaev <pochaev.nik@gmail.com>
2020-11-08 08:57:41 +0300

    Initial commit

---
+++ app/src/main/java/com/po4yka/androidacademy/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.po4yka.androidacademy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
