9c5a4dc8a76f5357ef41efaf6059a2d010e46e78
Nikita Pochaev <pochaev.nik@gmail.com>
2020-11-21 13:00:27 +0300

    Add buildSrc functionality & CI ktlint

---
+++ buildSrc/src/main/java/com/po4yka/androidacademy/buildsrc/dependencies.kt
@@ -0,0 +1,39 @@
+package com.po4yka.androidacademy.buildsrc
+
+object Versions {
+    const val ktlint = "0.39.0"
+}
+
+object Libs {
+    const val androidGradlePlugin = "com.android.tools.build:gradle:4.1.1"
+    const val junit = "junit:junit:4.13"
+
+    object Kotlin {
+        private const val version = "1.4.10"
+        const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$version"
+        const val gradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$version"
+        const val extensions = "org.jetbrains.kotlin:kotlin-android-extensions:$version"
+    }
+
+    object AndroidX {
+        const val coreKtx = "androidx.core:core-ktx:1.5.0-alpha04"
+
+        object Test {
+            private const val version = "1.2.0"
+            const val core = "androidx.test:core:$version"
+            const val rules = "androidx.test:rules:$version"
+
+            object Ext {
+                private const val version = "1.1.2-rc01"
+                const val junit = "androidx.test.ext:junit-ktx:$version"
+            }
+
+            const val espressoCore = "androidx.test.espresso:espresso-core:3.2.0"
+        }
+    }
+
+    object Design {
+        const val material = "com.google.android.material:material:1.2.1"
+        const val constrains = "com.android.support.constraint:constraint-layout:2.0.4"
+    }
+}
