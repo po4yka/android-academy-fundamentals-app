027e23c3873a3ad5020fd839790c4e7ed6f22388
Nikita Pochaev <pochaev.nik@gmail.com>
2020-11-21 16:26:59 +0300

    Add Movie Desc Activity

--- app/src/main/java/com/po4yka/androidacademy/MainActivity.kt
+++ app/src/main/java/com/po4yka/androidacademy/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.po4yka.androidacademy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val pushButton = findViewById<Button>(R.id.btn_open_movie_details)
+        pushButton.setOnClickListener {
+            startMovieDetailsActivity()
+        }
+    }
+
+    private fun startMovieDetailsActivity() {
+        val intent = Intent(this, MovieDetailsActivity::class.java)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/po4yka/androidacademy/MovieDetailsActivity.kt
@@ -0,0 +1,11 @@
+package com.po4yka.androidacademy
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MovieDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_details)
+    }
+}
