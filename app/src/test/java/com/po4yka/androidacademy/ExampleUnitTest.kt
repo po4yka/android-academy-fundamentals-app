78a63a973e4be272c6666ef0b0de6dc6ead86ef6
Nikita Pochaev <pochaev.nik@gmail.com>
2020-11-24 21:30:40 +0300

    All fixes from commits

--- app/src/test/java/com/po4yka/androidacademy/ExampleUnitTest.kt
+++ app/src/test/java/com/po4yka/androidacademy/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.po4yka.androidacademy
 
-import org.junit.Test
-
 import org.junit.Assert.*
+import org.junit.Test
 
 /**
  * Example local unit test, which will execute on the development machine (host).
